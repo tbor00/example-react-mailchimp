3f760618b2600ea17dc4f31b26a0c6473b7c60d8
tbor00 <btimoteo@outlook.com>
2021-06-01 14:58:25 -0300

    Done - example with send-suscriptor-mailchimp-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css"
+import "./App.css"
+import Newsletter from "send-suscriptor-mailchimp-react"
+import GithubCorner from 'react-github-corner';
 
-function App() {
+const App = () => {
+
+  const todo = {
+    formClassName: 'container mt-2', // Opcional - Optional
+    inputEmailClassName:"input-email", // Opcional - Optional
+    inputSubmitClassName: "btn btn-primary", // Opcional - Optional
+    valueSubmit : 'Suscribe!', // Opcional - Optional
+    listMailchimp: 'https://outlook.us6.list-manage.com/subscribe/post?u=5f8afe65a28ad5a1c9b1aae72&amp;id=1e9f07f5d8', // Obligatorio - Mandatory
+    messageSucces: 'Bien hecho! Ya estas suscripto', // Obligatorio - Mandatory
+    messageError: 'Hubo un error y no lo pudimos completar, tal vez es porque ya estas suscripto!', // Obligatorio - Mandatory
+    messageClassName: "text-center text-white pt-3" // Opcional - Optional
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <GithubCorner href="https://github.com/tbor00/send-suscriptor-mailchimp-react"/>
+    <div className="container">
+      <h2 className="text-white pt-5 text-center">Newsletter</h2>
+      <p className="text-white text-center">Suscribe in newsletter</p>
+      <div className="d-flex align-items-center justify-content-center">
+        <Newsletter info={todo}/>
+      </div>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
